1ecb524d288d5573d7717d00b3729de582b868a8
AjBhanat <ajayjbhanat@gmail.com>
2024-02-29 22:54:53 +0530

    Initial Commit

---
+++ lib/string_calclator_ruby_tdd.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "string_calclator_ruby_tdd/version"
+
+module StringCalclatorRubyTdd
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/string_calclator_ruby_tdd/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module StringCalclatorRubyTdd
+  VERSION = "0.1.0"
+end
